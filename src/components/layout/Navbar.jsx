b6511873704c08a25a468d2f85c90d9c6f06762a
Rajat Verma <rajatverma5885045@gmail.com>
2020-07-12 17:18:53 +0530

    add header

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -3,57 +3,71 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 
-const Navbar = (props) => {
-  const logout = () => {
-    props.logout();
-  };
+import './Navbar.css';
 
+const Navbar = (props) => {
   return (
     <div>
-      <div>
+      <div className="bg-gray-400 flex items-center p-2">
         <img
-          src="https://i.ibb.co/1rBQ1fr/1200px-Seal-of-Uttarakhand-svg.png"
+          className="h-10"
+          src={require('assets/govt-uk.png')}
           alt="govt. logo"
         />
-        <h2>
-          GOVERNMENT OF UTTARAKHAND <br />
-          <small>NATIONAL WEB PORTAL FOR JOBS</small>
-        </h2>
+        <p className="mx-2 text-xl font-medium">
+          GOVERNMENT OF UTTARAKHAND
+          <span className="text-base"> - NATIONAL WEB PORTAL FOR JOBS</span>
+        </p>
       </div>
 
-      <div>
-        <Link to="/">
-          <img src="https://i.ibb.co/FD0qzzT/awsar.png" alt="Awsar Logo" />
-        </Link>
-        <Link to="/alljobs">Jobs</Link>
-        <Link to="/">Organizations</Link>
-      </div>
+      <header className="flex items-center justify-between">
+        <div className="logo flex items-end text-lg font-bold">
+          <Link className="m-1" to="/">
+            <img
+              src={require('assets/awsar.png')}
+              className="h-10"
+              alt="Awsar Logo"
+            />
+          </Link>
+          <Link to="/jobs" className="mx-2 text-blue-700 hover:text-blue-800">
+            Jobs
+          </Link>
+          <Link to="/" className="text-blue-700 hover:text-blue-800">
+            Organizations
+          </Link>
+        </div>
 
-      <input id="nav" type="checkbox" />
-      <label htmlFor="nav" />
-
-      <nav>
-        <ul>
-          <li>
-            <Link to="/postresume">
-              <button>Post Resume</button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/hire">
-              <button>Post Job</button>
-            </Link>
-          </li>
-          <li>
-            <button onClick={logout} style={{ margin: '5px 8px' }}>
-              Logout
-            </button>
-            <Link to="/login">
-              <button>Login</button>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+        <input id="nav" type="checkbox" />
+        <label htmlFor="nav" />
+        <nav>
+          <ul>
+            <li className="mx-2">
+              <Link to="/postresume">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                  Post Resume
+                </button>
+              </Link>
+            </li>
+            <li className="mx-2">
+              <Link to="/hire">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                  Post Job
+                </button>
+              </Link>
+            </li>
+            <li className="ml-2">
+              {/* <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                Logout
+              </button> */}
+              <Link to="/login">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                  Login
+                </button>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
     </div>
   );
 };
