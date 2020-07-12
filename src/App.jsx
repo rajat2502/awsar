28010f122347494b4a49b3dfea1463b9f66eb4dc
Rajat Verma <rajatverma5885045@gmail.com>
2020-07-12 12:58:46 +0530

    add .env and pages setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import PrivateRoute from 'components/Commom/PrivateRoute';
+// import PrivateRoute from 'components/Commom/PrivateRoute';
 import Home from 'components/Home';
-import Profile from 'components/Profile/Profile';
-import Jobs from 'components/Jobs/index/';
-import Login from 'components/Login/Login';
+import Profile from 'components/Profile';
+import Jobs from 'components/Jobs';
+import Login from 'components/Login';
 import Signup from 'components/Signup';
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/" component={Home} />
-          <PrivateRoute exact path="/profile" component={Profile} />
+          <Route exact path="/profile" component={Profile} />
           <Route exact path="/jobs" component={Jobs} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={Signup} />
