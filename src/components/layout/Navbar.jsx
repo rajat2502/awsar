2cce7fc67f182cfa9df2a5cd6032b33121683430
Rajat Verma <rajatverma5885045@gmail.com>
2020-07-12 17:57:12 +0530

    add footer

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 
-import './Navbar.css';
+import './styles/Navbar.css';
 
 const Navbar = (props) => {
   return (
