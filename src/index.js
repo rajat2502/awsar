9184cf69f0a50afd9276d671e7005b27d954d311
Rajat Verma <rajatverma5885045@gmail.com>
2020-07-27 19:01:25 +0530

    pwa files add

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'styles/tailwind.css';
 
+import * as serviceWorker from './serviceWorker';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
+
+serviceWorker.register();
