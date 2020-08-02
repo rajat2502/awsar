bc12fe5df2de8f195f77c81333fbacc287934046
Rajat Verma <rajatverma5885045@gmail.com>
2020-08-02 12:18:12 +0530

    remove warnings

--- src/components/EmployeeDashboard/EmployeeDashboard.jsx
+++ src/components/EmployeeDashboard/EmployeeDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Redirect, Link } from 'react-router-dom';
 
 import { getEmployeeJobs, updateApplicationStatus } from 'api';
